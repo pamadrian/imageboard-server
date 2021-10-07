289a249aef3e3d84e87295111cb6eb24f549f532
pamadrian <pamvanalfen@icloud.com>
2021-10-07 18:23:08 +0200

    protected images

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const { toJWT, toData } = require("../auth/jwt");
 
 const router = new Router();
 
+// old login endpoint
 router.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -22,6 +23,24 @@ router.post("/login", async (req, res, next) => {
     next(e);
   }
 });
-// Here goes the login logic.
+
+// new login endpoint
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).send({
+      message: "Please supply a valid email and password",
+    });
+  } else {
+    // 1. find user based on email address
+    // 2. use bcrypt.compareSync to check the received password against the stored hash
+    // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+
+    res.send({
+      jwt: toJWT({ userId: 1 }),
+    });
+  }
+});
 
 module.exports = router;
--- routers/image.js
+++ routers/image.js
@@ -1,6 +1,7 @@
 const { Router, response } = require("express");
 const Image = require("../models").image;
 const router = new Router();
+const { toData } = require("../auth/jwt"); // because there is more exported, take that one thing you need.
 
 router.get("/", (request, response) => response.send([Image]));
 
@@ -16,6 +17,25 @@ router.get("/", async (request, response, next) => {
     const specificImage = await Image.findOne({ where: { title: "test" } });
   } catch (e) {
     console.error(e);
+  }
+});
+
+// Protecting images
+router.get("/auth/messy", async (req, res, next) => {
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(" ");
+  if (auth && auth[0] === "Bearer" && auth[1]) {
+    try {
+      const data = toData(auth[1]);
+      const allImages = await Image.findAll();
+      res.json(allImages);
+    } catch (e) {
+      res.status(400).send("Invalid JWT token");
+    }
+  } else {
+    res.status(401).send({
+      message: "Please supply some valid credentials",
+    });
   }
 });
 
